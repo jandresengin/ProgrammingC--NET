7e8f0ebd7f1fe5edd44cedb8ddd6cb6374765b87
JandresEngineer <jandresengineer@gmail.com>
2021-10-03 13:43:10 -0400

    Comments Added
    Comments Added

--- test01/AreaAndPerimeter/Form1.cs
+++ test01/AreaAndPerimeter/Form1.cs
@@ -8,6 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**************************************************************************************************************
+ * 
+ *      CSD2354 Programming C# NET          Instructor: Noah Petrides
+ *      Jairo Andres Supelano Rativa        c0812859
+ *      Calculate area and perimeter
+ * 
+ **************************************************************************************************************/
+
+
 namespace AreaAndPerimeter
 {
     public partial class Form1 : Form
@@ -28,19 +37,20 @@ namespace AreaAndPerimeter
          */
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            decimal diameterEntered = Convert.ToDecimal(txtDiameter.Text);
-            double radiusCircle = ((double)diameterEntered) / 2.0;
-            double areaCircle = Math.Pow(radiusCircle, 2) * Math.PI;
-            double perimeterCircle = 2 * Math.PI * radiusCircle;
+            decimal diameterEntered = Convert.ToDecimal(txtDiameter.Text); //The value entered by the user in the Diameter text box is taken, in this case it is converted from string to decimal according to the requirements of the problem.
+            double radiusCircle = ((double)diameterEntered) / 2.0; // The radius is calculated, where the saved value of diameter is taken but converting it to double and dividing by 2. With this we obtain greater precision.
+            double areaCircle = Math.Pow(radiusCircle, 2) * Math.PI; // The area is calculated, where the value of the radius calculated in the previous step is taken and raised to the power of 2, with the Math.Pow library and then multiplied by the value of pi, which is called by invoking the Math library. PI.
+            double perimeterCircle = 2 * Math.PI * radiusCircle; //In this step, the perimeter is only calculated taking the value of the radius and multiplied by 2, then by the value of PI invoking Math.PI.
 
-            txtArea.Text = areaCircle.ToString("f4");
-            txtPerimeter.Text = perimeterCircle.ToString("f4");
-            txtDiameter.Focus();
+            //Plot in the textbox with the data format.
+            txtArea.Text = areaCircle.ToString("f4"); // The value of the area is taken and converted to String, but it is indicated to show the result in F4 format (Float with 4 decimal places).
+            txtPerimeter.Text = perimeterCircle.ToString("f4"); //The value of the perimeter is taken and converted to String, but it is indicated to show the result in F4 format (Float with 4 decimal places).
+            txtDiameter.Focus(); // The focus is sent to the textbox txtDiameter
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close(); //This instruction executes the closing of the current form and the program.
         }
     }
 }
--- test01/ScoreCalculator/Form1.cs
+++ test01/ScoreCalculator/Form1.cs
@@ -8,6 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**************************************************************************************************************
+ * 
+ *      CSD2354 Programming C# NET          Instructor: Noah Petrides
+ *      Jairo Andres Supelano Rativa        c0812859
+ *      Accumulate test score data
+ * 
+ **************************************************************************************************************/
+
 namespace ScoreCalculator
 {
     public partial class Form1 : Form
@@ -17,8 +25,10 @@ namespace ScoreCalculator
             InitializeComponent();
         }
 
-        int scoreTotalTemp = 0;
-        int scoreCountTemp = 0;
+        int scoreTotalTemp = 0; // Variable scoreTotalTemp is declared with class scope, and initialized to 0. It is not inside any method. As it takes temporary values, it is left ending with the name Temp
+        int scoreCountTemp = 0; // Variable scoreCountTemp is declared with class scope, and initialized to 0. It is not inside any method. As it takes temporary values, it is left ending with the name Temp
+
+
         /******************************************************************************************************************************************************************
 		 *	☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸☸			⧆		🐘																			†
 		 *     Who is the most famous ant scientist?  Albert Antstein				⧆		🐘																			†
@@ -27,34 +37,41 @@ namespace ScoreCalculator
 		 */
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            int scoreTemp = Convert.ToInt32(txtScore.Text);
-            scoreTotalTemp += scoreTemp;
-            scoreCountTemp++;
-            double averageScore = (double)scoreTotalTemp / (double)scoreCountTemp;
-            txtScoreTotal.Text = scoreTotalTemp.ToString("d");
-            txtScoreCount.Text = scoreCountTemp.ToString("d");
-            txtAverage.Text = averageScore.ToString("f3");
 
-            txtScore.Text = "";
+            int scoreTemp = Convert.ToInt32(txtScore.Text); //The value entered by the user in the txtScore text box is taken, in this case it is converted from string to integer according to the requirements of the problem.
+            scoreTotalTemp += scoreTemp; //The value entered by the user is added to the value of scoreTotalTemp and will increase according to each new value entered by the user.
+            scoreCountTemp++; //This counter indicates the number of entries made by the user.
+            double averageScore = (double)scoreTotalTemp / (double)scoreCountTemp; //The average is calculated where the variables scoreTotalTemp (sum of all the numbers entered by the user so far) and scoreCountTemp (the number of entries made by the user so far) are converted into double, these two are divided and stored in a double variable called averageScore.
+
+
+            //Plot in the textbox with the data format.
+            txtScoreTotal.Text = scoreTotalTemp.ToString("d"); //The value of scoreTotalTemp is converted to a string and displayed in the txtScoreTotal textbox, with d format for integers
+            txtScoreCount.Text = scoreCountTemp.ToString("d"); //The value of scoreCountTemp is converted to a string and displayed in the txtScoreCount textbox, with d format for integers
+            txtAverage.Text = averageScore.ToString("f3"); //The value of averageScore is converted to a string and it is displayed in the txtAverage textbox, with f3 format (decimal number with 3 digits)
+
+
+            //The txtScore textbox is cleaned and the cursor is placed in this texbox so that the user continues entering numbers.
+            txtScore.Text = ""; 
             txtScore.Focus();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close(); //This instruction executes the closing of the current form and the program.
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            scoreTotalTemp = 0;
-            scoreCountTemp = 0;
+            
+            scoreTotalTemp = 0; //Variable scoreTotalTemp with class scope is initialized to 0.
+            scoreCountTemp = 0; //Variable scoreCountTemp with class scope is initialized to 0.
 
-            txtScore.Text = "";
-            txtScoreTotal.Text = "";
-            txtScoreCount.Text = "";
-            txtAverage.Text = "";
+            txtScore.Text = ""; //The txtScore textbox is cleared.
+            txtScoreTotal.Text = ""; //The txtScoreTotal textbox is cleared.
+            txtScoreCount.Text = ""; //The txtScoreCount textbox is cleared.
+            txtAverage.Text = ""; //The txtAverage textbox is cleared.
 
-            txtScore.Focus();
+            txtScore.Focus(); // The cursor is placed in the txtScore text box
 
         }
     }
