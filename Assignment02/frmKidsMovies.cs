68a100b4de8aec52a04c927945883f010c94fbd6
JandresEngineer <jandresengineer@gmail.com>
2021-10-18 23:19:59 -0400

    Commentes Added version 2
    Commentes Added version 2

--- Assignment02/frmKidsMovies.cs
+++ Assignment02/frmKidsMovies.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 /***********************************************************************************************************************
- *          CSD2354 Programming C# NET          Fall 2021       Assignment 1
+ *          CSD2354 Programming C# NET          Fall 2021       Assignment 2
  *          Jairo Andres Supelano               c0812859
  ************************************************************************************************************************/
 
@@ -16,7 +16,9 @@ namespace Assignment01
 {
     public partial class frmKidsMovies : Form
     {
+        //variable of the number of movies to return by the user. The initial value is 0.
         int numberMoviesEntered = 0;
+        // The total value to be paid by the customer without applying a discount is saved in the following variable. Initially it is left at 0.
         Double totalWithoutDiscount = 0;
         public frmKidsMovies()
         {
@@ -24,68 +26,75 @@ namespace Assignment01
             // DateTime is called to obtain the current date and the format is converted so that it can be read as month day year.
             // This is printed in the textbox txtCurrentDate
             txtCurrentDate.Text = DateTime.Now.ToString(@"MM-dd-yyyy");
+            // The number of movies to return is 0 and it is printed in the textbox
             txtNumberOfMovies.Text = numberMoviesEntered.ToString("d");
+            // The default value of the type of customer is New Customer "N". The user could change and apply for a better discount.
             txtTypeCustomer.Text = "N";
+            //The focus after load the form is over the dataTimepicker.
             dateTimePickerDueDate.Focus();
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            // The current date is stored in the variable dCurrent.
+            string selectedTypeCustomer = txtTypeCustomer.Text;//   The value entered by the client is taken, where you have the type of client that is and with it apply to discounts.  
+            double lateFeeBill = 0; //Variable is declared and initialized to 0.
+            decimal discountPercent = .0m; // This variable indicates the discount percentage to be applied to special customers.
+            int numberOfDaysLate; //This variable stores the number of days late in returning the movie.
 
-            string selectedTypeCustomer = txtTypeCustomer.Text;
-            double lateFeeBill = 0;
-            decimal discountPercent = .0m;
-            int numberOfDaysLate;
-            DateTime dCurrent = DateTime.Now;
-            DateTime dDue = dateTimePickerDueDate.Value;
-            TimeSpan totalNumberDays = (dCurrent.Date - dDue.Date);
-            double numberOfDays = totalNumberDays.TotalDays;
-            txtNumbersOfDaysLate.Text = numberOfDays.ToString();
+            DateTime dCurrent = DateTime.Now; // The current date is stored in the variable dCurrent.
+            DateTime dDue = dateTimePickerDueDate.Value; //According to the value entered in the dateTimePicker, the day is obtained and saved in dDue
+            TimeSpan totalNumberDays = (dCurrent.Date - dDue.Date); //The number of days late in returning the film is calculated according to the value entered by the user.
+            double numberOfDays = totalNumberDays.TotalDays; //Only the data of number of days is extracted from the variable type DateTime
+            txtNumbersOfDaysLate.Text = numberOfDays.ToString(); //The resulting number of days is shown in the textbox.
+
+            //The resulting number of days is taken, it is converted to an integer and it is verified that it is an adequate value to calculate
             bool success = Int32.TryParse(txtNumbersOfDaysLate.Text, out numberOfDaysLate);
             if (success)
             {
+                //If the number of days of delay is greater than or equal to 0, the associated costs will be calculated.
                 if (numberOfDaysLate >= 0)
                 {
                     // The rate to be charged is calculated where the number of days late is multiplied with the rate for movies with category New Releases,
-                    // which is 2 CAD per day.
+                    // which is 0.15 CAD per day.
 
-                    if (numberOfDaysLate != 0)
+                    if (numberOfDaysLate != 0) //If the number of days of delay is 0 (Due day = Current day), no surcharge is calculated, because the film is being delivered on the indicated date.
                     {
-                        numberMoviesEntered += 1;
-                        lateFeeBill = 0.15 * numberOfDaysLate;
-                        totalWithoutDiscount += lateFeeBill;
+                        numberMoviesEntered += 1; //The number of movies is increased by 1.
+                        lateFeeBill = 0.15 * numberOfDaysLate; //The surcharge is calculated for this type of film, which is 0.15 dollars for each day of delay.
+                        totalWithoutDiscount += lateFeeBill; //The previous value is added to the accumulator of the total of films to be returned, with this the user can calculate different films of this type and of different days.
                     }
 
-                    // The rate to be charged for the delay of the lateFeeBill variable is converted to String and formatted as currency
+                    // // The rate to be charged for the delay of the lateFeeBill variable is converted to String and formatted as currency
                     switch (selectedTypeCustomer)
                     {
-                        case "L":
+                        case "L":   //If you are an L Loyal user, a 10% discount is applied.
                             discountPercent = .1m;
                             break;
-                        case "J":
+                        case "J":   //If you are a J junior user, a 5% discount is applied.
                             discountPercent = .05m;
                             break;
-                        case "N":
+                        case "N":   //If you are an N New user, a 0% discount is applied.
                             discountPercent = 0;
                             break;
-                        default:
+                        default:   //If a different type of character is entered, a discount of 0% is applied, and a message is displayed to the user that in order to apply to any type of discount, he must select the type of user he is.
                             MessageBox.Show("To apply any kind of discount please select your Loyal customers ");
                             discountPercent = 0;
                             break;
                     }
+                    //The discount that the user has is calculated for the type of user that entered.
                     decimal discountAmount = Convert.ToDecimal(totalWithoutDiscount) * discountPercent;
+                    //The final value to be paid is calculated, after applying discounts.
                     decimal invoiceTotal = Convert.ToDecimal(totalWithoutDiscount) - discountAmount;
-                    txtNumberOfMovies.Text = numberMoviesEntered.ToString("d");
-                    txtLateFee.Text = lateFeeBill.ToString("c");
-                    subtotalWithoutDiscount.Text = totalWithoutDiscount.ToString("c");
-                    txtTotalWithDiscount.Text = invoiceTotal.ToString("c");
+                    txtNumberOfMovies.Text = numberMoviesEntered.ToString("d"); //The number of movies entered so far is graphed, it is changed to string format.
+                    txtLateFee.Text = lateFeeBill.ToString("c"); //The amount owed for the film that is delivered late is graphed, the format is changed to string currency.
+                    subtotalWithoutDiscount.Text = totalWithoutDiscount.ToString("c"); //The value owed for the films that were delivered late without discount is graphed (Acummulator), the format is changed to string currency.
+                    txtTotalWithDiscount.Text = invoiceTotal.ToString("c"); //The amount owed so far for all the films calculated with the applicable discount is graphed, the format is changed to string currency.
 
-                    btnReturn.Focus();
+                    btnReturn.Focus(); //The focus is moved to the return button
                 }
             }
             else
-            {
+            {   //In the event that the number of days late cannot be converted to an integer, a messagebox will be displayed indicating that the date was not entered correctly.
                 MessageBox.Show("The number of days introducted is not correct ' " + numberOfDaysLate + " '.");
             }
         }
@@ -96,8 +105,6 @@ namespace Assignment01
             this.Hide(); //the current form is hidden.
             formMain.ShowDialog(); //the main form is called.
             this.Close(); //At the end the current form is closed to not leave active forms or threads.
-
         }
-
     }
 }
