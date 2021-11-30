ec21be8ff80d9a16c1aeceef4c9165657a1251eb
JandresEngineer <jandresengineer@gmail.com>
2021-11-30 01:06:38 -0500

    Control fixed

--- Assignment04/frmNewRelease.Designer.cs
+++ Assignment04/frmNewRelease.Designer.cs
@@ -144,11 +144,15 @@ namespace Assignment04
             this.dateTimePickerDueDate.CustomFormat = "MM-dd-yyyy";
             this.dateTimePickerDueDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dateTimePickerDueDate.Location = new System.Drawing.Point(203, 31);
-            this.dateTimePickerDueDate.MaxDate = new System.DateTime(2021, 11, 29, 0, 0, 0, 0);
+            DateTime todaysDate = DateTime.Today;
+            int year = todaysDate.Year;
+            int month = todaysDate.Month;
+            int day = todaysDate.Day;
+            this.dateTimePickerDueDate.MaxDate = new System.DateTime(year, month, day, 0, 0, 0, 0);
             this.dateTimePickerDueDate.Name = "dateTimePickerDueDate";
             this.dateTimePickerDueDate.Size = new System.Drawing.Size(123, 22);
             this.dateTimePickerDueDate.TabIndex = 1;
-            this.dateTimePickerDueDate.Value = new System.DateTime(2021, 11, 29, 0, 0, 0, 0);
+            this.dateTimePickerDueDate.Value = new System.DateTime(year, month, day, 0, 0, 0, 0);
             // 
             // label5
             // 
@@ -229,6 +233,7 @@ namespace Assignment04
             this.comboBoxCustomerType.Name = "comboBoxCustomerType";
             this.comboBoxCustomerType.Size = new System.Drawing.Size(121, 24);
             this.comboBoxCustomerType.TabIndex = 3;
+            this.comboBoxCustomerType.SelectedIndex = 2;
             // 
             // btnClear
             // 
