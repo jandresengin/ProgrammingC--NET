ab0315d2310ef900d819de045e58606f8d7d51c1
JandresEngineer <jandresengineer@gmail.com>
2021-12-18 22:21:12 -0500

    Version 1

---
+++ test03/InventoryMaintenance/InventoryMaintenance/Plant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class Plant : InvItem
+    {
+        private string size;
+
+        public Plant()
+        {
+
+        }
+
+        public Plant(int itemNo, string description, decimal price, string size) : base(itemNo, description, price)
+        {
+            this.size = size;
+        }
+        public string Size
+        {
+            get
+            {
+                return this.size;
+            }
+            set
+            {
+                this.size = value;
+            }
+        }
+
+        public override string GetDisplayText() => base.GetDisplayText() + " (" + this.size + ")";
+
+
+    }
+}
---
+++ test03/InventoryMaintenance/InventoryMaintenance/Supply.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class Supply : InvItem
+    {
+        private string manufacturer;
+        public Supply()
+        {
+
+        }
+        public Supply(int itemNo, string description, decimal price, string manufacturer) : base(itemNo, description, price)
+        {
+            this.manufacturer = manufacturer;
+        }
+        public string Manufacturer
+        {
+            get
+            {
+                return this.manufacturer;
+            }
+            set
+            {
+                this.manufacturer = value;
+            }
+        }
+
+
+        public override string GetDisplayText() => base.GetDisplayText() + " (" + this.manufacturer + ")";
+
+
+
+    }
+
+
+
+
+}
